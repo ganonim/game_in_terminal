be7a3c0a892b9834a54dbe2bb901819e489de9ba
Uriy <jurasherepa@gmail.com>
2023-12-01 21:04:41 +0200

    game

--- main.py
+++ main.py
@@ -8,18 +8,27 @@ with open('data.json', 'r') as file:
 
 hero_x = 8
 hero_y = 8
+key_pressed = False
+
 
 while True:
-	if keyboard.is_pressed('up'):
+	if keyboard.is_pressed('up') and not key_pressed:
 		hero_y = hero_y-1
-	elif keyboard.is_pressed('down'):
+		key_pressed = True
+	if keyboard.is_pressed('down') and not key_pressed:
 		hero_y = hero_y+1
-	elif keyboard.is_pressed('left'):
+		key_pressed = True
+	elif keyboard.is_pressed('left') and not key_pressed:
 		hero_x = hero_x-1
-	elif keyboard.is_pressed('right'):
+		key_pressed = True
+	elif keyboard.is_pressed('right') and not key_pressed:
 		hero_x = hero_x+1
+		key_pressed = True
 	elif keyboard.is_pressed('esc'):
 		exit()
+
+	elif not keyboard.is_pressed('up') and not keyboard.is_pressed('down') and not keyboard.is_pressed('left') and not keyboard.is_pressed('right'):
+		key_pressed = False
 
 	os.system('clear')
 	data[hero_y][hero_x] = "@"
@@ -30,4 +39,4 @@ while True:
 
 	with open('data.json', 'r') as file:
 		data = json.load(file)
-	time.sleep(0.1)
+	#time.sleep(0.1)
