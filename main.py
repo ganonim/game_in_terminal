2fa89404fdb52c95afe1d0a1b1e41f1699b8a588
Uriy <jurasherepa@gmail.com>
2023-12-03 00:23:05 +0200

    game

--- main.py
+++ main.py
@@ -10,20 +10,23 @@ KEY_PRESSED = False
 GAME_START = True
 
 with open('data.json', 'r', encoding="utf-8") as file:
-    DATA = json.load(file)
-
+    data = json.load(file)
 
 def graphics():
     os.system('clear')
-    DATA[HERO_Y][HERO_X] = "@"
-    for i, DATA_MAP in enumerate(DATA):
+    data[HERO_Y][HERO_X] = "@"
+    for i, DATA_MAP in enumerate(data):
         print(*DATA_MAP)
 
 
 def move_hero(dx, dy):
-    global HERO_X, HERO_Y
-    HERO_X += dx
-    HERO_Y += dy
+    global HERO_X, HERO_Y, data
+    new_x = HERO_X + dx
+    new_y = HERO_Y + dy
+    if 0 <= new_x < len(data[0]) and 0 <= new_y < len(data):
+        HERO_X = new_x
+        HERO_Y = new_y
+        graphics()
 
 
 while True:
@@ -31,26 +34,27 @@ while True:
         GAME_START = False
         graphics()
 
-    if keyboard.is_pressed('up') and not KEY_PRESSED:
-        move_hero(0, -1)
-        KEY_PRESSED = True
-        graphics()
-    if keyboard.is_pressed('down') and not KEY_PRESSED:
-        move_hero(0, 1)
-        KEY_PRESSED = True
-        graphics()
-    elif keyboard.is_pressed('left') and not KEY_PRESSED:
-        move_hero(-1, 0)
-        KEY_PRESSED = True
-        graphics()
-    elif keyboard.is_pressed('right') and not KEY_PRESSED:
-        move_hero(1, 0)
-        KEY_PRESSED = True
-        graphics()
-    elif keyboard.is_pressed('esc'):
+    if not KEY_PRESSED:
+        if keyboard.is_pressed('up'):
+            move_hero(0, -1)
+            KEY_PRESSED = True
+        elif keyboard.is_pressed('down'):
+            move_hero(0, 1)
+            KEY_PRESSED = True
+        elif keyboard.is_pressed('left'):
+            move_hero(-1, 0)
+            KEY_PRESSED = True
+        elif keyboard.is_pressed('right'):
+            move_hero(1, 0)
+            KEY_PRESSED = True
+        if KEY_PRESSED:
+            graphics()
+    if keyboard.is_pressed('esc'):
+        print("good byeee~~~")
         sys.exit()
+
     if not any(keyboard.is_pressed(key) for key in KEYS_TO_CHECK):
         KEY_PRESSED = False
 
     with open('data.json', 'r', encoding="utf-8") as file:
-        DATA = json.load(file)
+        data = json.load(file)
